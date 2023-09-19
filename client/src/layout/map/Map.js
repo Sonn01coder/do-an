460c8ac6061cca0362303a5fcf09ea081aa6e1e3
son.nguyentruong <son.nguyentruong@ncc.asia>
2023-09-19 17:37:40 +0700

    19/09/23

--- client/src/layout/map/Map.js
+++ client/src/layout/map/Map.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { LOCATION_HANOI } from '../../shared/constants/Constants'
 import { Icon } from 'leaflet';
@@ -8,10 +8,13 @@ import { VillageContext } from '../../shared/dataContext/VillageContext';
 import { POIContext } from '../../shared/dataContext/PointOfInterestContext';
 import { Link, useParams } from 'react-router-dom';
 import ChangeMap from './ChangeMap';
+import axios from 'axios';
 
 
 
 export default function Map() {
+  const [point, setPoint] = useState([])
+
   const {villages} = useContext(VillageContext)
   const {poi} = useContext(POIContext)
 
@@ -32,6 +35,45 @@ export default function Map() {
     const geocode = JSON.parse(village.geocode);
     return geocode
   }
+
+//   const getNearbyPlaces = async (location, type, radius, apiKey) => {
+//     try {
+//       const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, {
+//         params: {
+//           location: `${location.lat},${location.lng}`,
+//           radius: radius,
+//           type: type,
+//           key: apiKey
+//         }
+//       });
+      
+//       console.log(response.data.results);
+//       return response.data.results;
+//     } catch (error) {
+//       console.error('Lỗi khi lấy dữ liệu từ Google Places API:', error);
+//     }
+//   };
+
+
+//   const location = { lat:20.97680019333064 , lng: 105.91003709350106 }; 
+//   const radius = 1000; 
+//   const apiKey = 'AIzaSyA5KoMADDMYxIGDSGhkdm2DF2VbtW3lvoU';
+
+
+// useEffect(() => {
+//   if(slug) {
+//     const fetchData = async () => {
+//       const result = await getNearbyPlaces(location, 'restaurant', radius, apiKey);
+//       console.log(result);
+//     }
+
+//      fetchData()
+//   } 
+      
+// }, [slug]);
+
+
+console.log(point);
 
   return (
     <div className='map'>
